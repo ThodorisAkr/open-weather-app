b4a1ee0644f07610aeb18a75e41f5f983d010de0
Thodoris Akritidis <thodoris.akritidis98@gmail.com>
2021-11-27 13:28:54 +0200

    add bg images and title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Open Weather App</h1>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
