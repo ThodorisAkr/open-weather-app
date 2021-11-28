b8a263930e7a61245289060129f0e036e932fc28
Thodoris Akritidis <thodoris.akritidis98@gmail.com>
2021-11-28 16:44:48 +0200

    footer changes

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -1,6 +1,10 @@
 import classes from "./Footer.module.css";
 
 const Footer = () => {
-  return <footer className={classes.footer}>Theodoros Akritidis © 2021</footer>;
+  return (
+    <footer className={classes.footer}>
+      Theodoros Akritidis © 2021 Open Weather API
+    </footer>
+  );
 };
 export default Footer;
